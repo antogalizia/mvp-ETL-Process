79954e3191105b4ddcb8f853b89989fed5b52504
antogalizia <galiziaantonella9@gmail.com>
2023-07-25 18:02:58 +0100

    Moved the files rename.

--- Storage.py
+++ Storage.py
@@ -14,9 +14,10 @@ class Storage:
     # I define the new file path will correspond to the current month
     directory = paths.project_destiny + f'\{month}'
 
+
     def store(self):
-        route_downloads = Directories.origin
-        route_destiny = Directories.project_destiny
+        route_downloads = paths.origin
+        route_destiny = paths.project_destiny
 
         if not os.path.isfile(route_downloads):
             print("File not found")
@@ -25,28 +26,15 @@ class Storage:
         if os.path.isdir(route_destiny):
             shutil.move(route_downloads, route_destiny)
 
-        os.chdir(paths.project_destiny)
+        os.chdir(route_destiny)
 
         # I create the directory and move the file into it
         if not os.path.exists(Storage.directory):
             os.makedirs(Storage.month)
             shutil.move(paths.csv_file_path, Storage.directory)
 
-        os.chdir(Storage.directory)
-        month_day = Storage.current_day.day
 
-        # I rename the file according to the week
-        match month_day:
-            case 7:
-                os.rename("ventas.csv", "SPW1.csv")
-            case 14:
-                os.rename("ventas.csv", "SPW2.csv")
-            case 21:
-                os.rename("ventas.csv", "SPW3.csv")
-            case 28:
-                os.rename("ventas.csv", "SPW4.csv")
-            case _:
-                os.rename("ventas.csv", "SR.csv")
+
 
 
 
